c3585883e2f75bbc5646aac9c3b2f8637190e97f
oxygen <oxygen5307@gmail.com>
2019-04-02 22:58:01 +0800

    Add code

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask, request, abort
+from urllib.request import urlopen
+#from oauth2client.service_account import ServiceAccountCredentials
 
 from linebot import (
     LineBotApi, WebhookHandler
 )
 from linebot.exceptions import (
-    InvalidSignatureError
+    InvalidSignatureError,LineBotApiError
 )
+
+################################
+
 from linebot.models import *
 
 app = Flask(__name__)
@@ -33,7 +38,22 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
+    print(event)
+    text=event.message.text
+
+    if (text=="Hi"):
+        reply_text = "Hello"
+        #Your user ID
+
+    elif(text=="你好"):
+        reply_text = "哈囉"
+    elif(text=="機器人"):
+        reply_text = "叫我嗎"
+    else:
+        reply_text = text
+#如果非以上的選項，就會學你說話
+
+    message = TextSendMessage(reply_text)
     line_bot_api.reply_message(event.reply_token, message)
 
 import os
