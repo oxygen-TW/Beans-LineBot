1d9930dc87973e839c7a6097e3f0db98c8ff2ecc
oxygen <oxygen5307@gmail.com>
2019-04-04 16:47:02 +0800

    Add code

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, abort
 import requests, json
 
-from functions import MakeRailFall
 from hanziconv import HanziConv
 
 from linebot import (
@@ -62,6 +61,25 @@ def MakeWeather(station):
     msg += MakeAQI(station)
     return msg
 
+def MakeRailFall(station):
+    result = requests.get("https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0002-001?Authorization=rdec-key-123-45678-011121314")
+    msg = "沒有這個測站啦"
+
+    if(result.status_code != 200):
+        return "雨量資料讀取失敗"
+    else:
+        railFallData = result.json()
+        for item in railFallData["records"]["location"]:
+            if station in item["locationName"]:
+                msg += "目前雨量："+ item["weatherElement"][7]["elementValue"] + "mm\n"
+                if item["weatherElement"][3]["elementValue"] == "-998.00":
+                    msg += "三小時雨量：0.00mm\n"
+                else:
+                    msg += "三小時雨量：" + item["weatherElement"][3]["elementValue"] + "mm\n"
+                msg += "日雨量：" + item["weatherElement"][6]["elementValue"] + "mm\n"
+                return msg
+        return msg
+        
 def MakePoem():
     APIURL = "http://gxy.me/tangshi?format=json"
     r = requests.get(APIURL)
