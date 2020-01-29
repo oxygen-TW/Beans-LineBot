cf354ed2230521c7c7d42e7e68da692e0ed0eb44
oxygen-TW <howard2805@gmail.com>
2020-01-29 21:31:16 +0800

    remove bugs

--- gold_price.py
+++ gold_price.py
@@ -7,9 +7,9 @@ class GoldPrice():
         url = "https://rate.bot.com.tw/xrt?Lang=zh-TW"
         html = requests.get(url).text
 
-        cash_Buy = float(re.search(r"本行現金買入.+>([0-9+.]+)", html)[1])
-        cash_sell = float(re.search(r"本行現金賣出.+>([0-9+.]+)", html)[1])
-        buy = float(re.search(r"本行即期買入.+>([0-9+.]+)", html)[1])
-        sell = float(re.search(r"本行即期賣出.+>([0-9+.]+)", html)[1])
+        cash_Buy = re.search(r"本行現金買入.+>([0-9+.]+)", html)[1]
+        cash_sell = re.search(r"本行現金賣出.+>([0-9+.]+)", html)[1]
+        buy = re.search(r"本行即期買入.+>([0-9+.]+)", html)[1]
+        sell = re.search(r"本行即期賣出.+>([0-9+.]+)", html)[1]
 
         return {"本行現金買入":cash_Buy,"本行現金賣出":cash_sell,"本行即期買入":buy,"本行即期賣出.":sell}
