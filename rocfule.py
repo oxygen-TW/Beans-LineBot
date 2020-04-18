bb08992da143c3ccb85ef55c00db1ea1d9280733
oxygen-TW <howard2805@gmail.com>
2020-04-19 01:01:30 +0800

    Fix roc fule.

--- rocfule.py
+++ rocfule.py
@@ -3,6 +3,11 @@
 import re
 import requests
 from bs4 import BeautifulSoup
+import logging
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(levelname)s %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
 
 def getFuelPrice():
     url = "https://www2.moeaboe.gov.tw/oil102/oil2017/newmain.asp"
@@ -20,8 +25,22 @@ def getFuelPrice():
     return price
 
 def GetPredictPrice():
-    url = "https://toolbxs.com/zh-TW/detector/gasoline_price"
-    result = requests.get(url)
+    url = "https://gas.goodlife.tw/"
+    headers = {
+        'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1'
+    }
+
+    result = requests.get(url, headers=headers)
     soup = BeautifulSoup(result.content, 'html.parser')
-    tag = soup.find_all("p", class_=["prediction down","up"])
-    return tag[0].string
+    tag = soup.find_all("h2", class_=["down","up"])
+    logging.debug(tag[0]["class"][0])
+    price = tag[0].em.string
+
+    if(tag[0]["class"][0] == "down"):
+        return  "降" + price + "元"
+    elif(tag[0]["class"][0] == "up"):
+        return "漲" + price + "元"
+    else:
+        return "讀取錯誤，請聯絡開發人員"
+
+logging.debug(GetPredictPrice())
--- test.py
+++ test.py
@@ -1,5 +1,22 @@
 
 from config import *
 from rocfule import *
+import re
+import requests
+from bs4 import BeautifulSoup
 
-GetPredictPrice()
+url = "https://gas.goodlife.tw"
+headers = {
+    'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
+    "accept": r"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "accept-encoding": "gzip, deflate, br",
+    "accept-language":"zh-TW",
+    "authority": "toolbxs.com", "method": "GET", "path": r"/zh-TW/detector/gasoline_price", "scheme": "https"
+}
+
+result = requests.get(url)
+result.encoding = "gbk18030"
+
+soup = BeautifulSoup(result.text ,"html.parser",from_encoding="iso-8859-1")
+print(soup)
+print(soup.original_encoding)
